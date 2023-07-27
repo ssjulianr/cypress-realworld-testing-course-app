e01a7704388eabc6c6959f6c5b22761fe050f8e5
Julian <julian@Administrators-MacBook-Pro.local>
2023-07-27 08:58:50 -0400

    spotlight update

---
+++ cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,17 @@
+describe('Newsletter Subscribe Form', () => {
+    beforeEach(() =>{
+        cy.visit('http://localhost:3000')
+    })
+
+    it('Allows users to subscribe to email list', () => {
+        cy.getByData('email-input').type('tom@aol.com')
+        cy.getByData('submit-button').click()
+        cy.getByData('success-message').should('exist').contains('tom@aol.com')
+    })
+
+    it('does NOT allow an invalid email address', () => {
+        cy.getByData('email-input').type('tom')
+        cy.getByData('submit-button').click()
+        cy.getByData('success-message').should('not.exist')
+    })
+})
